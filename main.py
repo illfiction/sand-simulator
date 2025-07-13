27934a223400b72c062a4eae35d0d6d9816fc7c5
illfiction <atharva.sonwane@outlook.com>
2025-07-13 11:32:32 +0530

    added gravity

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ placement_delay = 50  # milliseconds
 last_updated_time = 0
 update_delay = 40
 
+gravity = 0.2
+
+pygame.time.Clock()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,20 +36,28 @@ while True:
         gy = pos[1] // CELL_SIZE
 
         if 0 <= gx < GRID_WIDTH and 0 <= gy < GRID_HEIGHT:
-            grid[gy][gx] = (255, 255, 0)
+            grid[gy][gx] = {"color" : (255, 255, 0), "vy" : 1.0}
             last_placed_time = pygame.time.get_ticks()
 
     if pygame.time.get_ticks() - last_updated_time > update_delay:
         for y in range(GRID_HEIGHT - 2, -1, -1):
             for x in range(GRID_WIDTH):
-                if grid[y][x] and grid[y+1][x] is None:
-                    grid[y+1][x] = grid[y][x]
-                    grid[y][x] = None
-                elif grid[y][x] and grid[y+1][x] is not None:
-                    direction = random.choice([-1, 1])
-                    if 0 <= x + direction < GRID_WIDTH and grid[y+1][x+direction] is None:
-                        grid[y+1][x+direction] = grid[y][x]
-                        grid[y][x] = None
+                if grid[y][x]:
+                    vy = grid[y][x]["vy"]
+                    max_new_y = y + int(vy)
+
+                    for new_y in range(max_new_y, y, -1):
+                        if 0 <= new_y < GRID_HEIGHT and grid[new_y][x] is None:
+                            grid[y][x]["vy"] += gravity
+                            grid[new_y][x] = grid[y][x]
+                            grid[y][x] = None
+                            break
+
+                    if grid[y][x] and grid[y+1][x]:
+                        direction = random.choice([-1, 1])
+                        if 0 <= x + direction < GRID_WIDTH and grid[y+1][x+direction] is None:
+                            grid[y+1][x+direction] = grid[y][x]
+                            grid[y][x] = None
 
         last_updated_time = pygame.time.get_ticks()
 
@@ -53,7 +65,7 @@ while True:
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
             if grid[y][x]:
-                pygame.draw.rect(screen, grid[y][x],
+                pygame.draw.rect(screen, grid[y][x]['color'],
                                  (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     pygame.display.flip()
