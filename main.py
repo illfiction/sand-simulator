10bd98e1725fab85a004569e1fb5f35de14ecf95
illfiction <atharva.sonwane@outlook.com>
2025-07-14 17:03:58 +0530

    add keyboard shortcuts

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 from sand_texture_loader import *
 
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
+font = pygame.font.SysFont(None, 24)
+screen = pygame.display.set_mode((600, 600))
 
 pygame.display.set_caption("Sand-Simulator")
 
@@ -23,15 +24,38 @@ gravity = 0.2
 
 pygame.time.Clock()
 
-palette, probabilities = load_palette_and_weights()
+sand_palette, sand_probs = load_palette_and_weights("sand")
+gravel_palette, gravel_probs = load_palette_and_weights("gravel")
+
+materials = {
+    "sand": {"name": "Sand","palette": sand_palette, "prob": sand_probs},
+    "gravel": {"name": "Gravel", "palette": gravel_palette, "prob": gravel_probs},
+}
+active_material = materials["sand"]
+status_text = ""
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:   #this is if player presses closes tab then the program quits
             pygame.quit()
             quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                active_material = materials["sand"]
+            elif event.key == pygame.K_2:
+                active_material = materials["gravel"]
+            elif event.key == pygame.K_c:
+                grid = [[None for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+            elif event.key == pygame.K_f:
+                if update_delay != 40:
+                    update_delay = 40
+                    status_text = ""
+                else:
+                    update_delay = 200000000000
+                    status_text = "Time: Frozen"
 
-    screen.fill((0, 0, 0))  #fills screen with black background
+
+    screen.fill((0, 0, 0))  #fills screen with a black background
 
     if pygame.mouse.get_pressed()[0] and pygame.time.get_ticks() - last_placed_time > placement_delay:
         #checking if mouse left click is pressed and a delay timer so the clicks are not too fast
@@ -39,22 +63,25 @@ while True:
         gx = pos[0] // CELL_SIZE
         gy = pos[1] // CELL_SIZE
 
-        if 5 <= gx < GRID_WIDTH - 4  and 5 <= gy < GRID_HEIGHT - 4:
+        if 0 <= gx < GRID_WIDTH  and 0 <= gy < GRID_HEIGHT:
             for y in range(-5,5):
                 for x in range(-5,5):
-                    grid[gy+y][gx+x] = {"color" : random.choices(palette, weights=probabilities)[0], "vy" : 1.0}    #places yellow square with velocity 1(if velocity is less than 1 then block will not move
-                    last_placed_time = pygame.time.get_ticks()      #updated timer
+                    if 0 <= gx+x < GRID_WIDTH and 0 <= gy+y < GRID_HEIGHT:
+                        if grid[gy+y][gx+x] is None:
+                            grid[gy+y][gx+x] = {"color" : random.choices(active_material["palette"], weights=active_material["prob"])[0], "vy" : 1.0}    #places active material with velocity 1(if velocity is less than 1 then block will not move
+                            last_placed_time = pygame.time.get_ticks()      #updated timer
 
     if pygame.mouse.get_pressed()[2]:
         pos = pygame.mouse.get_pos()
         gx = pos[0] // CELL_SIZE
         gy = pos[1] // CELL_SIZE
 
-        if 5 <= gx < GRID_WIDTH - 5 and 5 <= gy < GRID_HEIGHT - 5:
+        if 0 <= gx < GRID_WIDTH  and 0 <= gy < GRID_HEIGHT:
             for y in range(-5,5):
                 for x in range(-5,5):
-                    if grid[y+gy][x+gx]:
-                        grid[y+gy][x+gx] = None
+                    if 0 <= gx+x < GRID_WIDTH and 0 <= gy+y < GRID_HEIGHT:
+                        if grid[y+gy][x+gx]:
+                            grid[y+gy][x+gx] = None
 
 
     if pygame.time.get_ticks() - last_updated_time > update_delay:
@@ -90,4 +117,8 @@ while True:
                 pygame.draw.rect(screen, grid[y][x]['color'],
                                  (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
+    text = font.render("Material: " + active_material["name"], True, (255, 255, 255))
+    screen.blit(text, (10, 10))
+    status = font.render(status_text, True, (255, 255, 255))
+    screen.blit(status, (10, 30))
     pygame.display.flip()
--- sand_texture_loader.py
+++ sand_texture_loader.py
@@ -2,7 +2,10 @@ from PIL import Image
 from collections import Counter
 img = Image.open("textures/sand.png").convert('RGB')
 
-def load_palette_and_weights():
+def load_palette_and_weights(material):
+    image_str = f"textures/{material}.png"
+
+    img = Image.open(image_str).convert('RGB')
     pixels = list(img.getdata())
 
     color_counts = Counter(pixels)
